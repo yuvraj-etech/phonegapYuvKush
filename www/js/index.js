d4d0eeda359b7ff701c22a16ef6efd69b0e05af5
ujjwal-etech <ujjawal@excellencetechnologies.in>
2015-11-25 15:37:13 +0530

    Dialouge for Application Version - kush

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 angular.module('networkApp', ['ngCordova'])
         .controller('mainCtrl', function($scope, $rootScope, $cordovaNetwork, 
-            $cordovaAppVersion,$cordovaDevice) {
+            $cordovaAppVersion,$cordovaDevice,$cordovaDialogs) {
            
             document.addEventListener("deviceready", function() {
                 $scope.var = "Thank You";
@@ -18,13 +18,18 @@ angular.module('networkApp', ['ngCordova'])
                     $scope.status = "Offline";
                     $scope.networkType = $cordovaNetwork.getNetwork();
                     $scope.nType = $scope.networkType;
-                });
-                
+                }); 
                 $scope.appVersn=AppVersion.version;
                 console.log(AppVersion.version);  
                 console.log(AppVersion.build); 
                 $scope.appBuild=AppVersion.build;
-
+                $scope.get=function()
+                {
+                $cordovaDialogs.alert('Application Version',$scope.appVersn,'ok')
+                .then(function() {
+                console.log($scope.appVersn);
+                });
+                } ; 
 
                 $scope.device = $cordovaDevice.getDevice();
 
