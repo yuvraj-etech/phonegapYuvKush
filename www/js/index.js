dcbf87dc2fa547fbb4ec1854b545159dc22dbdf8
ujjwal-etech <ujjawal@excellencetechnologies.in>
2015-11-25 14:13:41 +0530

    Application Version - kush

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,31 @@
-angular.module('networkApp',['ngCordova']).controller('mainCtrl', function($scope,$rootScope, $cordovaNetwork) {
-console.log("bammmmmm");
-    document.addEventListener("deviceready", function () {
-  $scope.var="Thank You";
-console.log($scope.isOnline);
+angular.module('networkApp', ['ngCordova']).controller('mainCtrl', function($scope, $rootScope, $cordovaNetwork, $cordovaAppVersion {
+            console.log("bammmmmm");
+            document.addEventListener("deviceready", function() {
+                $scope.var = "Thank You";
+                console.log($scope.isOnline);
 
-$rootScope.$on('$cordovaNetwork:online', function(event, networkState){
-            console.log("went online");
-            $scope.status = "Online";
-            $scope.networkType=$cordovaNetwork.getNetwork();
-            $scope.nType=$scope.networkType;
-          });
- 
-          $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
-            console.log("went offline");
-             $scope.status = "Offline";
-             $scope.networkType=$cordovaNetwork.getNetwork();
-            $scope.nType=$scope.networkType;
-          });
+                $rootScope.$on('$cordovaNetwork:online', function(event, networkState) {
+                    console.log("went online");
+                    $scope.status = "Online";
+                    $scope.networkType = $cordovaNetwork.getNetwork();
+                    $scope.nType = $scope.networkType;
+                });
 
-}, false);
+                $rootScope.$on('$cordovaNetwork:offline', function(event, networkState) {
+                    console.log("went offline");
+                    $scope.status = "Offline";
+                    $scope.networkType = $cordovaNetwork.getNetwork();
+                    $scope.nType = $scope.networkType;
+                });
+                $cordovaAppVersion.getVersionNumber().then(function(version) {
+                    console.log(version);
+                    $scope.appVersn = version;
+                });
+                $cordovaAppVersion.getVersionCode().then(function(build) {
+                    console.log(build);
+                    $scope.appBuild = build;
+                });
 
-});
+            }, false);
+
+        });
